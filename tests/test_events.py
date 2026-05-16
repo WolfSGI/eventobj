f2d72432b7e0669a69088e9e6dea8683c96a12d1
Souheil Chelfouh <trollfot@gmail.com>
2026-05-16 17:37:48 +0200

    Added ObjectEvent tests

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,14 +1,16 @@
 from unittest.mock import Mock, call
-from eventobj import Event, Events
+from eventobj import Event, ObjectEvent, Events
 
 
 def test_basic_usecase():
     events = Events()
     mock = Mock()
 
+
     @events.register(Event)
     def very_simple_handler(event):
         mock.touch(event)
+
 
     event = Event()
     events.notify(event)
@@ -36,6 +38,7 @@ def test_sorted_events():
     def handler3(event):
         mock.touch('handler3')
 
+
     event = Event()
     events.notify(event)
 
@@ -57,7 +60,43 @@ def test_basic_inheritence():
     def very_simple_handler(event):
         mock.touch(event)
 
+
     event = SomeEvent()
     events.notify(event)
 
     mock.touch.assert_called_with(event)
+
+
+def test_object_event():
+
+    events = Events()
+    mock = Mock()
+
+
+    @events.register(ObjectEvent, object)
+    def handler_for_all(event):
+        mock.touch(f'generic {event.obj}')
+
+
+    @events.register(ObjectEvent, str)
+    def handler2_for_str(event):
+        mock.touch(f'str {event.obj}')
+
+
+    @events.register(ObjectEvent, int)
+    def handler3_for_int(event):
+        mock.touch(f'int {event.obj}')
+
+
+    event = ObjectEvent("42")
+    events.notify(event)
+
+    mock.touch.assert_has_calls(
+        (call("generic 42"), call("str 42")), any_order=True
+    )
+    mock.reset()
+
+    event = ObjectEvent(42)
+    events.notify(event)
+
+    mock.touch.assert_has_calls((call("int 42"),))
