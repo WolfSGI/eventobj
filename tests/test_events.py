c991b0a97efe11af6038f43b76d19bc3c6f5d902
Souheil Chelfouh <trollfot@gmail.com>
2026-05-16 18:59:35 +0200

    Added merging tests

--- tests/test_events.py
+++ tests/test_events.py
@@ -5,18 +5,17 @@ from eventobj import Event, ObjectEvent, Events
 def test_basic_usecase():
     events = Events()
     mock = Mock()
-
+    assert len(events) == 0
 
     @events.register(Event)
     def very_simple_handler(event):
         mock.touch(event)
 
+    assert len(events) == 1
 
     event = Event()
     events.notify(event)
-
     mock.touch.assert_called_with(event)
-
 
 
 def test_sorted_events():
@@ -39,6 +38,7 @@ def test_sorted_events():
         mock.touch('handler3')
 
 
+    assert len(events) == 3
     event = Event()
     events.notify(event)
 
@@ -100,3 +100,49 @@ def test_object_event():
     events.notify(event)
 
     mock.touch.assert_has_calls((call("int 42"),))
+
+
+def test_events_merging():
+
+    events1 = Events()
+    events2 = Events()
+
+    @events1.register(ObjectEvent, str)
+    def handler_for_str(event):
+        mock.touch(f'str {event.obj}')
+
+
+    @events1.register(ObjectEvent, int)
+    def handler_for_int(event):
+        mock.touch(f'int {event.obj}')
+
+
+    @events2.register(ObjectEvent, str)
+    def other_handler_for_str(event):
+        mock.touch(f'other str {event.obj}')
+
+
+    @events2.register(ObjectEvent, int)
+    def other_handler_for_int(event):
+        mock.touch(f'other int {event.obj}')
+
+
+    assert len(events1) == 2
+    assert len(events2) == 2
+
+    events = events1 | events2
+    assert len(events) == 4
+
+    mock = Mock()
+    event = ObjectEvent("42")
+    events.notify(event)
+    mock.touch.assert_has_calls(
+        (call("str 42"), call("other str 42")), any_order=True
+    )
+
+    mock.reset()
+    event = ObjectEvent(42)
+    events.notify(event)
+    mock.touch.assert_has_calls(
+        (call("int 42"), call("other int 42")), any_order=True
+    )
